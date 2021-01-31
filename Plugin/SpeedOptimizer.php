8ce06241b42a6d2bb834b34c09601bbd75bbd579
Alex Dong <support@alothemes.com>
2021-01-31 22:58:03 +0700

    Update SpeedOptimizer.php
    Fixed error break line tags <textarea> and <pre>

--- Plugin/SpeedOptimizer.php
+++ Plugin/SpeedOptimizer.php
@@ -2,8 +2,8 @@
 /**
  * @Author: nguyen
  * @Date:   2020-02-12 14:01:01
- * @Last Modified by:   nguyen
- * @Last Modified time: 2021-01-31 22:02:48
+ * @Last Modified by:   Alex Dong
+ * @Last Modified time: 2021-01-31 22:56:43
  */
 
 namespace Magepow\SpeedOptimizer\Plugin;
@@ -331,6 +331,16 @@ class SpeedOptimizer extends \Magento\Framework\View\Element\Template
 
     public function minifyHtml($content) 
     {
+        $minHtml = $content;
+
+        // Searching textarea and pre
+        preg_match_all('#\<textarea.*\>.*\<\/textarea\>#Uis', $minHtml, $foundTxt);
+        preg_match_all('#\<pre.*\>.*\<\/pre\>#Uis', $minHtml, $foundPre);
+
+        // replacing both with <textarea>$index</textarea> / <pre>$index</pre>
+        $minHtml = str_replace($foundTxt[0], array_map(function($el){ return '<textarea>'.$el.'</textarea>'; }, array_keys($foundTxt[0])), $minHtml);
+        $minHtml = str_replace($foundPre[0], array_map(function($el){ return '<pre>'.$el.'</pre>'; }, array_keys($foundPre[0])), $minHtml);
+
         $search = array(
             '/\>[^\S ]+/s',     // strip whitespaces after tags, except space
             '/[^\S ]+\</s',     // strip whitespaces before tags, except space
@@ -347,7 +357,12 @@ class SpeedOptimizer extends \Magento\Framework\View\Element\Template
             // ''
         );
 
-        $minHtml = preg_replace($search, $replace, $content);
+        $minHtml = preg_replace($search, $replace, $minHtml);
+
+        // Replacing back with content
+        $minHtml = str_replace(array_map(function($el){ return '<textarea>'.$el.'</textarea>'; }, array_keys($foundTxt[0])), $foundTxt[0], $minHtml);
+        $minHtml = str_replace(array_map(function($el){ return '<pre>'.$el.'</pre>'; }, array_keys($foundPre[0])), $foundPre[0], $minHtml);
+
         /* Return $content when $minHtml empty */
         return $minHtml ? $minHtml : $content;
     }
